888f76c002903a8ead40517e143eb3a59d486b98
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-07-24 15:34:24 -0300

    fix/ atualiza a api de /tarefas para /task na URL

--- api-tarefas/src/server.ts
+++ api-tarefas/src/server.ts
@@ -6,7 +6,7 @@ const PORTA = 3333;
 
 app.use(express.json()); 
 
-app.use('/tarefas', taskRoutes);
+app.use('/task', taskRoutes);
 
 app.listen(PORTA, () => {
   console.log(`🚀 Servidor rodando na porta ${PORTA}`);

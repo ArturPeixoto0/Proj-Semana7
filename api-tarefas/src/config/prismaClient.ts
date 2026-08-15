de6d8ec02e59fe0f83a3dfe66a41046ec2143228
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-15 17:21:21 -0300

    feat/ atualiza o prismaClient em relação ao novo .env e adiciona mariadb

--- api-tarefas/src/config/prismaClient.ts
+++ api-tarefas/src/config/prismaClient.ts
@@ -1,7 +1,14 @@
-// src/config/prismaClient.ts
 import 'dotenv/config'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 import { PrismaClient } from '../../generated/prisma/client'
 
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL!)
+// Passe as opções de conexão diretamente no construtor do adaptador
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: Number(process.env.DATABASE_PORT) || 3306,
+  user: process.env.DATABASE_USER || 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+})
+
 export const prisma = new PrismaClient({ adapter })

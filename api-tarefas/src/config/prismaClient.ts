eac6fac1d495f7e5b4806843bbb0d24d33411296
Artur Peixoto <arturpeixoto140706@gmail.com>
2026-08-16 13:55:48 -0300

    feat/ corrige o prismaClient considerando o .env atualizado

--- api-tarefas/src/config/prismaClient.ts
+++ api-tarefas/src/config/prismaClient.ts
@@ -1,14 +1,16 @@
-import 'dotenv/config'
-import { PrismaMariaDb } from '@prisma/adapter-mariadb'
-import { PrismaClient } from '../generated/prisma/client/index.js'
+import 'dotenv/config';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../generated/prisma/client/index.js';
 
-// Passe as opções de conexão diretamente no construtor do adaptador
+const databaseUrl = new URL(process.env.DATABASE_URL!);
+
 const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: Number(process.env.DATABASE_PORT) || 3306,
-  user: process.env.DATABASE_USER || 'root',
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-})
+  host: databaseUrl.hostname,
+  port: Number(databaseUrl.port),
+  user: databaseUrl.username,
+  password: decodeURIComponent(databaseUrl.password),
+  database: databaseUrl.pathname.substring(1),
+  connectionLimit: 5,
+});
 
-export const prisma = new PrismaClient({ adapter })
+export const prisma = new PrismaClient({ adapter });
